51d0574f19b40258696404824b3db371e3d34cf5
Ross Carran <carran.ross@gmail.com>
2023-01-29 07:50:56 +1300

    Fist pass Working Mod Transpiler made and functioning Notes added

--- BTPathMod/BTSavePathMod.cs
+++ BTPathMod/BTSavePathMod.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
 using BattleTech;
+using BattleTech.Save.Core;
 using HarmonyLib;
 using HBS.Logging;
 
@@ -12,32 +16,50 @@ namespace BTPathMod
         public static void Start()
         {
             s_log.Log("Starting");
-            s_log.Log(GamePath());
-
+         
             Harmony.CreateAndPatchAll(typeof(BTSavePathMod));
         
             s_log.Log("Started");
         }
-    
-        private static string GamePath()
-        {
-            var myPath = Directory.GetCurrentDirectory();
-            return myPath;
-        }
-
+        
+        /**
+         * Transpiler that redirects Gameinstance Initialisation so it goes through ChangeLocalSaveFolder First
+         */
+        [HarmonyDebug]
         [HarmonyPatch(typeof(UnityGameInstance), nameof(UnityGameInstance.Reset))]
-        [HarmonyPostfix]
+        [HarmonyTranspiler]
         [HarmonyAfter("io.github.ross-carran.BattleTechTools")]
-        public static void GetSaveGamePath(UnityGameInstance ___instance)
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            s_log.Log(___instance.Game.SaveManager.saveSystem.localWriteLocation.rootPath);
-            ___instance.Game = Modified(new GameInstance());
+            var codes = new List<CodeInstruction>(instructions);
+            foreach (var t in codes)
+            {
+                if (t.ToString().Contains("BattleTech.UnityGameInstance::set_Game(BattleTech.GameInstance value)"))
+                {
+                    t.opcode = OpCodes.Callvirt;
+                    MethodInfo methTesting = SymbolExtensions.GetMethodInfo(() => ChangeLocalSaveFolder(new GameInstance()));
+                    yield return new CodeInstruction(OpCodes.Call, methTesting);
+                }
+                yield return t;
+            }
         }
-
-        public static GameInstance Modified(GameInstance patchInstance)
+        
+        /**
+         * This method generates the new path for the save folder
+         * Currently This changes the path to the users BattleTech game folder then adds the user inputted folder
+         * resulting in somefilestructure\{battletechgamefolder}\{userdefinedsavefolder} system decides weather / or \ is used
+         * Path.DirectorySeperatorChar is used to make sure the path is generated properly regardless of operating system.
+         * Cloud save Location is left unmodified as the game shits the bed if it is changed.
+         * Will more then likely make a settings class to give more control over this process
+         */
+        public static GameInstance ChangeLocalSaveFolder(GameInstance game)
         {
+            var userDir = "ModSaves";
+            var strlocal = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + userDir + Path.DirectorySeparatorChar + SaveSystem.StandaloneFolder;
             
-            return patchInstance;
+            game.SaveManager.saveSystem.localWriteLocation.rootPath = strlocal;
+
+            return game;
         }
     }
 }
