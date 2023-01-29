a069dc653f9d4b1d81c2bdab3f22884c490d1d00
Ross Carran <carran.ross@gmail.com>
2023-01-30 03:23:21 +1300

    got save and cleanup finished for profiles.dat

--- BTPathMod/BTSavePathMod.cs
+++ BTPathMod/BTSavePathMod.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using BattleTech;
@@ -15,6 +16,7 @@ namespace BTPathMod
     public static class BTSavePathMod
     {
         private static readonly ILog s_log = Logger.GetLogger(nameof(BTPathMod));
+        
         public static void Start()
         {
             s_log.Log("Starting");
@@ -28,7 +30,6 @@ namespace BTPathMod
         /**
          * Transpiler that redirects Gameinstance Initialisation so it goes through ChangeLocalSaveFolder First
          */
-        [HarmonyDebug]
         [HarmonyPatch(typeof(UnityGameInstance), nameof(UnityGameInstance.Reset))]
         [HarmonyTranspiler]
         [HarmonyAfter("io.github.ross-carran.BattleTechTools")]
@@ -46,32 +47,33 @@ namespace BTPathMod
                 yield return t;
             }
         }
-
+        
         [HarmonyPatch(typeof(ProfileManager), nameof(ProfileManager.Save))]
         [HarmonyTranspiler]
         [HarmonyAfter("io.github.ross-carran.BattleTechTools")]
         static IEnumerable<CodeInstruction> Transpiler2(IEnumerable<CodeInstruction> instructions)
         {
-            var codes = new List<CodeInstruction>();
-            return codes;
+            var codes = new List<CodeInstruction>(instructions);
+            return CodeReuse(codes);
         }
-
+        
+        [HarmonyDebug]
         [HarmonyPatch(typeof(ProfileManager), nameof(ProfileManager.LoadSaveData))]
         [HarmonyTranspiler]
         [HarmonyAfter("io.github.ross-carran.BattleTechTools")]
         static IEnumerable<CodeInstruction> Transpiler3(IEnumerable<CodeInstruction> instructions)
         {
-            var codes = new List<CodeInstruction>();
-            return codes; 
+            var codes = new List<CodeInstruction>(instructions);
+            return codes;
         }
 
         [HarmonyPatch(typeof(ProfileManager), nameof(ProfileManager.ClearProfiles))]
         [HarmonyTranspiler]
         [HarmonyAfter("io.github.ross-carran.BattleTechTools")]
-        static IEnumerable<CodeInstruction> Transpiler4(IEnumerable<CodeInstruction> instruction)
+        static IEnumerable<CodeInstruction> Transpiler4(IEnumerable<CodeInstruction> instructions)
         {
-            var codes = new List<CodeInstruction>();
-            return codes;       
+            var codes = new List<CodeInstruction>(instructions);
+            return CodeReuse(codes);      
         }
 
         public static GameInstance ChangeLocalSaveFolder(GameInstance game)
@@ -83,11 +85,27 @@ namespace BTPathMod
 
         public static string PathStringGenerator(string saveSystemFolderPath)
         {
-            return Settings.RootSaveDirPath +
+            return Settings.RootSaveDirPath() +
                    Path.DirectorySeparatorChar +
                    Settings.UserDefinedDirectory +
                    Path.DirectorySeparatorChar +
                    saveSystemFolderPath;
+        }
+
+        private static IEnumerable<CodeInstruction> CodeReuse(IEnumerable<CodeInstruction> myInstructions)
+        {
+            var codes = new List<CodeInstruction>(myInstructions);
+            for (var i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].ToString().Contains("static string UnityEngine.Application::get_persistentDataPath()"))
+                {
+                    MethodInfo myMeth = SymbolExtensions.GetMethodInfo((() => Settings.RootSaveDirPath()));
+                    codes [i] = new CodeInstruction(OpCodes.Call, myMeth);
+                    s_log.Log("target Found");
+                }
+                s_log.Log(codes[i].ToString());
+            }
+            return codes;
         }
     }
 }
